a93ad2ea862401ecdbde70a9781e001d3238dc8d
mtvbrianking <mtvbrianking@gmail.com>
2018-12-31 10:37:45 +0300

    Routing

--- public/index.php
+++ public/index.php
@@ -16,4 +16,42 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = new \App\Application();
 
-var_dump($app->getContainer());
+/*
+|--------------------------------------------------------------------------
+| Router; "nikic/fast-route"
+|--------------------------------------------------------------------------
+*/
+
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $router) {
+    $router->addRoute('GET', '/', function() {
+        print "Hello world";
+    });
+});
+
+// Fetch method and URI from somewhere
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+$uri = $_SERVER['REQUEST_URI'];
+
+// Strip query string (?foo=bar) and decode URI
+if (false !== $pos = strpos($uri, '?')) {
+    $uri = substr($uri, 0, $pos);
+}
+$uri = rawurldecode($uri);
+
+$routeInfo = $dispatcher->dispatch($httpMethod, $uri);
+switch ($routeInfo[0]) {
+    case FastRoute\Dispatcher::NOT_FOUND:
+        throw new \Exception("404 - Not Found");
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        throw new \Exception("405 - Method Not Allowed");
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $reflection = new \ReflectionFunction($handler);
+        if($reflection->isClosure()) {
+            $reflection->invoke();
+        }
+        break;
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 PHP DIY Micro Framework
 --
 
-I'm going to build a micro framework as a way to learn and appreciate PHP advanced features like Dependency Injection.
+**Dependency Injection**: [Pimple](https://pimple.symfony.com/)
 
-But mostly; am trying to understand what goes on under the hood in the frameworks we use, in my case; Laravel.
+**Routing**: [FastRoute](https://github.com/nikic/FastRoute)
